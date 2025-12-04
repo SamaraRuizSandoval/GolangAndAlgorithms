f454a80436d123b4e4ff22323f3cb25ff600c779
Samara Ruiz Sandoval <rsandoval.samara@gmail.com>
2025-12-04 18:41:39 -0500

    Worked with dependency injection

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	dependencyinjection "github.com/SamaraRuizSandoval/GolangAndAlgorithms/fundamentals/dependency_injection"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	dependencyinjection.Greet(os.Stdout, "Elodie")
 }
