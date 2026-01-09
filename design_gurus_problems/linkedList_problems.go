4200a24cde31fa7c123a6dc68b94816549469320
Samara Ruiz Sandoval <rsandoval.samara@gmail.com>
2026-01-09 15:24:37 -0500

    Solves MergeTwoLists problem

--- design_gurus_problems/linkedList_problems.go
+++ design_gurus_problems/linkedList_problems.go
@@ -5,14 +5,12 @@ import (
 )
 
 //? Given a sorted linked list, remove all the duplicate elements to leave only distinct numbers. The linked list should remain sorted, and the modified list should be returned.
-// Input: N = 2
-// Output: ["1", "10"]
+// Input: 1 -> 2 -> 2 -> 3
+// Output: 1 -> 2 -> 3
 
 func DeleteDuplicates(head *linkedlist.ListNode) *linkedlist.ListNode {
-	// Initialize the current node as the head of the list.
 	current := head
 
-	// Traverse through the list until the end is reached.
 	for current != nil && current.Next != nil {
 		// If the next node is a duplicate, bypass it.
 		if current.Next.Val == current.Val {
@@ -23,6 +21,33 @@ func DeleteDuplicates(head *linkedlist.ListNode) *linkedlist.ListNode {
 		}
 	}
 
-	// Return the modified head of the list.
 	return head
+}
+
+//? Given the head of two sorted linked lists, l1 and l2. Return a new sorted list created by merging together the nodes of the first two lists.
+// Input: l1 = [1, 3, 5] , l2 [2, 4, 6]
+// Output: [1, 2, 3, 4, 5, 6]
+
+func MergeTwoLists(l1 *linkedlist.ListNode, l2 *linkedlist.ListNode) *linkedlist.ListNode {
+	newNode := &linkedlist.ListNode{}
+	current := newNode
+
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			current.Next = l1
+			l1 = l1.Next
+		} else {
+			current.Next = l2
+			l2 = l2.Next
+		}
+		current = current.Next
+	}
+
+	if l1 != nil {
+		current.Next = l1
+	} else {
+		current.Next = l2
+	}
+
+	return newNode.Next
 }
--- design_gurus_problems/linkedList_problems_test.go
+++ design_gurus_problems/linkedList_problems_test.go
@@ -43,3 +43,52 @@ func TestRemoveDuplicates(t *testing.T) {
 		})
 	}
 }
+
+type InputLists struct {
+	l1 linkedlist.ListNode
+	l2 linkedlist.ListNode
+}
+
+type MergeTwoListsTest struct {
+	name  string
+	input InputLists
+	want  string
+}
+
+var testMergeTwoLists = []MergeTwoListsTest{
+	{name: "Example 1", input: InputLists{*makeList(1, 3, 5), *makeList(2, 4, 6)}, want: "1 → 2 → 3 → 4 → 5 → 6 → "},
+	{name: "Example 2", input: InputLists{*makeList(1), *makeList(2, 3, 4)}, want: "1 → 2 → 3 → 4 → "},
+	{name: "Example 3", input: InputLists{*makeList(2, 4, 6), *makeList(1, 3, 5)}, want: "1 → 2 → 3 → 4 → 5 → 6 → "},
+}
+
+func TestMergeTwoLists(t *testing.T) {
+	for _, tt := range testMergeTwoLists {
+		t.Run(tt.name, func(t *testing.T) {
+			result := MergeTwoLists(&tt.input.l1, &tt.input.l2)
+			list := linkedlist.SinglyLinkedList{Head: result}
+
+			buffer := bytes.Buffer{}
+			list.Traverse(&buffer)
+
+			got := buffer.String()
+
+			if got != tt.want {
+				t.Errorf("got %q but want %q", got, tt.want)
+			}
+
+		})
+	}
+}
+
+func makeList(vals ...int) *linkedlist.ListNode {
+	if len(vals) == 0 {
+		return nil
+	}
+	head := &linkedlist.ListNode{Val: vals[0]}
+	curr := head
+	for _, v := range vals[1:] {
+		curr.Next = &linkedlist.ListNode{Val: v}
+		curr = curr.Next
+	}
+	return head
+}
